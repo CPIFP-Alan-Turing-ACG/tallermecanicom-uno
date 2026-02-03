65e7104245f6f82815e1113e55d579f97c693ae7
Carlos Aragonés Martín <caramar221@cpifpalanturing.es>
2026-02-03 13:14:07 +0100

    commit2

--- src/GestorVehiculos.java
+++ src/GestorVehiculos.java
@@ -1,6 +1,6 @@
 public class GestorVehiculos {
     public void registrarVehiculo() {
         // TODO: Implementar lógica (Alumno A)
-        System.out.println("Opel corsa se esta vendiendo");
+        System.out.println("Opel corsa vendido");
     }
 }
